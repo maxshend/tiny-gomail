d8e192992fe28c20765609855ff66c63e6cc1b67
maxshend <shendrik.mv@gmail.com>
2020-12-14 00:03:38 +0300

    Add implementation for SendHTMLMessage.

--- server/server.go
+++ server/server.go
@@ -8,24 +8,30 @@ import (
 	pb "github.com/maxshend/tiny_gomail/tinygomail"
 )
 
+const htmlMIME = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
+
 type mailServer struct {
 	pb.UnimplementedTinyGomailServer
 }
 
-func (m *mailServer) SendTextMessage(ctx context.Context, em *pb.EmailMessage) (*pb.SendResponse, error) {
-	auth := smtp.PlainAuth("", os.Getenv("SMTP_EMAIL"), os.Getenv("SMTP_PASSWORD"), os.Getenv("SMTP_HOST"))
-	msg := []byte("Subject: " + em.Subject + "\r\n" + em.Body + "\r\n")
+func (m *mailServer) SendTextMessage(ctx context.Context, em *pb.EmailMessage) (response *pb.SendResponse, err error) {
+	err = sendSMTPEmail(em, "")
 
-	err := smtp.SendMail(os.Getenv("SMTP_HOST")+":"+os.Getenv("SMTP_PORT"), auth, os.Getenv("SMTP_EMAIL"), em.To, msg)
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
+	return
 }
 
-func (m *mailServer) SendHTMLMessage(ctx context.Context, em *pb.EmailMessage) (*pb.SendResponse, error) {
-	return nil, nil
+func (m *mailServer) SendHTMLMessage(ctx context.Context, em *pb.EmailMessage) (response *pb.SendResponse, err error) {
+	err = sendSMTPEmail(em, htmlMIME)
+
+	return
+}
+
+func sendSMTPEmail(em *pb.EmailMessage, mime string) (err error) {
+	msg := []byte("Subject: " + em.Subject + "\r\n" + mime + "\r\n" + em.Body + "\r\n")
+	auth := smtp.PlainAuth("", os.Getenv("SMTP_EMAIL"), os.Getenv("SMTP_PASSWORD"), os.Getenv("SMTP_HOST"))
+	err = smtp.SendMail(os.Getenv("SMTP_HOST")+":"+os.Getenv("SMTP_PORT"), auth, os.Getenv("SMTP_EMAIL"), em.To, msg)
+
+	return
 }
 
 func main() {}
